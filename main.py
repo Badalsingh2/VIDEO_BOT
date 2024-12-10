47d7ba47b3dd185147ce734c22f65a11df114313
Badalsingh2 <Badalubentu@gmail.com>
2024-12-10 19:22:08 +0530

    Final

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from watchdog.events import FileSystemEventHandler
 # Constants
 VIDEO_DIR = "./videos"
 API_BASE_URL = "https://api.socialverseapp.com/posts"
-FLIC_TOKEN = "Your token"
+FLIC_TOKEN = "<Token>"
 HEADERS = {
     "Flic-Token": FLIC_TOKEN,
     "Content-Type": "application/json"
